341ec8fb6beab50ccc7297b3d0695a0c4a08c294
LucasgcTi <lucasgomescunha132@gmail.com>
2026-08-27 15:25:56 -0300

    add ex1 M, N, O

---
+++ EX1N.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(void)
+{
+    int A, B, C, R;
+
+    cout << "Insira o valor de <A>..: "; cin >> A;
+    cin.ignore(80, '\n');
+
+    cout << endl;
+
+    cout << "Insira o valor de <B>..: "; cin >> B;
+    cin.ignore(80, '\n');
+
+    cout << endl;
+
+    cout << "Insira o valor de <C>..: "; cin >> C;
+    cin.ignore(80, '\n');
+
+    R = pow(A + B + C, 2);
+
+    cout << endl << endl;
+
+    cout << "O resultado da soma dos quadrados e...: " << R;
+
+    cout << endl << endl;
+
+    cout << "Aperte <Enter> para encerrar...: ";
+    cin.get();
+
+    return 0;
+}
