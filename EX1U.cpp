221b299d6ef8e4abd78eb8110a708a2d17897436
LucasgcTi <lucasgomescunha132@gmail.com>
2026-08-28 15:36:17 -0300

    add ex S, T, U

---
+++ EX1U.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(void)
+{
+    float VOLUME, RAIO;
+
+    cout << "Insira o valor de Raio...: "; cin >> RAIO;
+    cin.ignore(80, '\n');
+    cout << endl;
+
+    VOLUME = (4/3) * 3.14159 * pow(RAIO, 3);
+
+    cout << "O valor do Volume é: " << VOLUME;
+
+    cout << endl << endl
+
+    cout << "Aperte <Enter> para encerrar... ";
+    cin.get()
+
+    return 0;
+}
